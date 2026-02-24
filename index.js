268e565eb24f6fd4e4262a1fbd5cae0c9b3241d9
HatafR <rizqi.fatah12345@gmail.com>
2026-02-24 14:22:06 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express()
+
+app.use((req, res, next) => {
+//   console.log(`Request ${req.path} - harus lewat sini`);
+  if (false) {
+    next(new Error('Not Authorized'));
+    return;
+  }
+  next();
+});
+
+ app.get('/', (req, res) =>{
+res.send('Fatah Rizqi | Lionel Messi Champion Of The World!');
+});
+
+// app.get('/', (req, res) => {
+//     res.send("OK");
+// });
+
+app.use((req, res, next) => {
+  console.log(`Request ${req.path} - harus lewat sini`);
+  next();
+});
+
+app.use((err, req, res, next) =>{
+res.send('Error Occurred');
+});
+
+app.listen(3000)
